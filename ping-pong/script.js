2bdb6a5e5224dd60a75ab236f15e0cb8af7effe7
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2022-11-26 06:59:25 -0300

    style pong

---
+++ ping-pong/script.js
@@ -0,0 +1,9 @@
+const items = {
+    buttonStart: document.querySelector('.start'),
+    ball: document.querySelector('.ball'),
+    player: document.querySelector('.left-bar'),
+    cpu: document.querySelector('.right-bar'),
+}
+
+let {buttonStart, ball, cpu, player} = items
+
